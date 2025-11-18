875623293843136a8426df252fe19777c42a21a3
marcos_framinan <framinanvideiramarcos@gmail.com>
2025-11-18 09:49:48 +0100

    se termina la condicional, indicando que  si se divide en 4 y es igual a 0 y dividido entre 100 distinto de 0 o es divisible entre 400 y de 0 será bisiesto, si no, no lo será y se indica que imprima esa solucion

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,5 @@
 anio = int(input("Introduce un año: "))
 if (anio % 4 == 0 and anio % 100 != 0) or (anio % 400 == 0):
     print(f"El año {anio} es bisiesto.")
+else:
+    print(f"El año {anio} no es bisiesto.")
