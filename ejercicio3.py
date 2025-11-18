18090ddeb5b2f07af085d9a939130a8360bbe0db
marcos_framinan <framinanvideiramarcos@gmail.com>
2025-11-18 10:04:41 +0100

    se marca la formula y se calculan las iteraciones

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,3 +1,9 @@
-itirenaciones = 10000
+iteraciones = 10000
 sol = 0.0
+for k in range(iteraciones):
+    termino = ((-1) ** k) / (2 * k + 1)
+    sol += termino
 
+pi_aprox = 4 * sol
+
+print(f"Aproximación de π con {iteraciones} iteraciones: {pi_aprox}")
