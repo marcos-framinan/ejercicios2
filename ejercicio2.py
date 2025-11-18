97de0e3c892660a9f7870137b50c807450750331
marcos_framinan <framinanvideiramarcos@gmail.com>
2025-11-18 09:58:19 +0100

    se pide que se introduzca un numero entero,  y lo calcula los except detectan errores como numeros negativos o que no sean enteros o que noi esten previstos

--- ejercicio2.py
+++ ejercicio2.py
@@ -9,3 +9,13 @@ def doble_factorial(n):
     for i in range(n, 0, -2):
         sol *= i
     return sol
+try:
+    numero = int(input("Introduce un número entero: "))
+    sol = doble_factorial(numero)
+    print(f"El doble factorial de {numero} es: {sol}")
+
+except ValueError as e:
+    print(f"Error: {e}")
+
+except Exception as e:
+    print(f"Ocurrió un error inesperado: {e}")
